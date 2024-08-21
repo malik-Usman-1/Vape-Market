d0bac223db05f0fd5c808322fea0dfa643cc9492
malik-Usman-1 <ujamshaid62@gmail.com>
2024-08-22 00:05:32 +0500

    add

--- src/Contact/Contact1.jsx
+++ src/Contact/Contact1.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { IoMail } from "react-icons/io5";
+import { MdLocationOn } from "react-icons/md";
+import { IoCall } from "react-icons/io5";
 
 const Contact1 = () => {
   return (
@@ -7,9 +10,33 @@ const Contact1 = () => {
     <div className=' bg-black  sm:px-14 px-5'>
         <div className=' grid lg:grid-cols-2 grid-cols-1 gap-10 py-24'>
         <div className='relative '>
-    <img src="../assets/img/01.jpg" alt="" className="w-full  " />
-    <div className='absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
-        <p className='text-white text-xl'>Your Text Here</p>
+    <img src="../assets/img/01.jpg" alt="" className="w-full  h-[540px]  md:h-[605px] " />
+    <div className='absolute inset-0 bg-black bg-opacity-50 md:space-y-14 space-y-6 py-10  md:px-8 px-4 '>
+        <h2 className=' text-white md:text-4xl text-2xl'>Have something in mind?
+        Let's talk.</h2>
+        <p className='text-white md:text-xl text-lg'>Adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim.</p>
+        <img src="../assets/img/arry.png" alt="" />
+        <div className=' space-y-4'>
+            <div className=' flex gap-3 items-center'>
+                <div className=' h-10 w-10 bg-white bg-opacity-30 flex justify-center items-center'>
+                <MdLocationOn className='  text-2xl  text-white ' />
+                </div>
+                <p className=' text-white md:text-lg text-base'>Afzal road sanda kalan Lahore.</p>
+            </div>
+            <div className=' flex gap-3 items-center'>
+                <div className=' h-10 w-10  bg-white bg-opacity-30 flex justify-center items-center'>
+                <IoCall  className='  text-2xl  text-white '/>
+                </div>
+                <p className=' text-white md:text-lg text-base'>03164466401</p>
+            </div>
+            <div className=' flex gap-3 items-center'>
+                <div className=' h-10 w-10 bg-white bg-opacity-30 flex justify-center items-center'>
+                <IoMail className='  text-2xl  text-white ' />
+                </div>
+                <p className=' text-white md:text-lg text-base'>ujamshaid62@gmail.com</p>
+            </div>
+        </div>
     </div>
 </div>
             <div className='  sm:space-y-12 space-y-6'>
@@ -31,6 +58,19 @@ const Contact1 = () => {
 
             </div>
 
+        </div>
+        <div>
+            <div className=' md:h-[600px] h-[400px] pb-10'>
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13597.927820728059!2d74.283311555091!3d31.565828937087296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39191cba0ea21c81%3A0xecb349ffcbf759a6!2sIslampura%2C%20Lahore%2C%20Punjab%2054000%2C%20Pakistan!5e0!3m2!1sen!2s!4v1724263898302!5m2!1sen!2s" 
+                width="100%" 
+                 className=' h-full'
+                style={{ border: 0 }} 
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="no-referrer-when-downgrade">
+            </iframe>
+            </div>
         </div>
     </div>
   </div>
